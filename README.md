d0ac8b1483564d0ae13131d03b3828682cb10d84
Ibrahim Ozturk <ibrahimozturk35@yandex.com>
2022-05-08 22:59:31 +0300

    Add ShiftCihper.js

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ShiftCipher
+
+A shift cipher takes a plain text message and shifts each letter forward in the alphabet by a given number. For example, a shift cipher with a shift of 1 would turn the string 'hello' to 'ifmmp'.
+
+ShiftCipher class takes the numerical value of the shift as a constructor parameter. The class have two methods:
+
+ *  encrypt: takes a plain text string and returns a capitalized string with each letter shifted forward in the alphabet based on the set shift value.
+
+ *  decrypt: takes an encrypted message and returns a lower case string with each letter shifted back in the alphabet based on the set shift value.
+
+ *  In both methods, any character outside the alphabet remains the same.
+
+ *  But if a character is shifted outside the alphabet in either direction it is wrapped around to the other side. For example, encrypting a y with a shift of 4 results in C and decrypting an A with a shift of 1 result in z.
---
+++ ShiftCipher.js
@@ -0,0 +1,57 @@
+// Write class below
+class ShiftCipher {
+  constructor(shiftBy) {
+    this._shiftBy = shiftBy;
+  }
+
+  encrypt(string) {
+    let encryptStr = '';
+    for (let i = 0; i < string.length; i++) {
+        if (charInAlphabet(string[i])) {
+          let shiftedChar = string.toUpperCase().charCodeAt(i) + this._shiftBy;
+
+          //Wrap the shifted char if out of alphabet
+          if (shiftedChar > 'Z'.charCodeAt(0)) {
+            shiftedChar -= 26; 
+          }
+          encryptStr += String.fromCharCode(shiftedChar);
+        }
+        else {
+            encryptStr += string[i];
+        }
+    }
+    return encryptStr;
+  }
+
+  decrypt(string) {
+    let decryptStr = '';
+    for (let i = 0; i < string.length; i++) {
+      if (charInAlphabet(string[i])) {
+          let shiftedChar = string.toLowerCase().charCodeAt(i) - this._shiftBy;
+
+          //Wrap the shifted char if out of alphabet
+          if (shiftedChar < 'a'.charCodeAt(0)) {
+            shiftedChar += 26; 
+          }
+          decryptStr += String.fromCharCode(shiftedChar);
+        }
+        else {
+          decryptStr += string[i];
+        }
+    }
+    return decryptStr;
+  }
+}
+
+const charInAlphabet = char => {
+  if ((char >= 'A' && char <= 'Z') || (char >= 'a' && char <= 'z')) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+const cipher = new ShiftCipher(2);
+console.log(cipher.encrypt('i love to code!'));// should return 'K NQXG VQ EQFG!'
+console.log(cipher.decrypt('K <3 OA RWRRA'));// should return 'i <3 my puppy'
